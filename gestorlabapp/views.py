ac3b9d1cdebcc6491c53cead1e032bd05d08369d
Renildo Rabi <renildorabi22@gmail.com>
2022-05-08 20:27:16 -0300

    Create #18

--- gestorlabapp/views.py
+++ gestorlabapp/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from gestorlabapp.forms import LabForm
+from gestorlabapp.models import Lab
 
 def home(request):
-    return render(request,'index.html')
+    data = {}
+    data['db'] = Lab.objects.all()
+    return render(request,'index.html',data)
 
 def form(request):
     data = {}
     data['form'] = LabForm()
     return render(request,'form.html',data)
+
+
+def create(request):
+    form = LabForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
