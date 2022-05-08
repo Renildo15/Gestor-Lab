40a3eb48340198100fce1a506599c9a7a3557fa7
Renildo Rabi <renildorabi22@gmail.com>
2022-05-08 17:32:55 -0300

    criação dos templates index e form #18

--- gestorlabproject/settings.py
+++ gestorlabproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ce(lge4i-1l6ka*sntzer3n9#vri2p3d3sl27gtvqtt8ebj2!'
+SECRET_KEY = 'django-insecure-=g#y0_2l!o$zt45-+e3jsh3p%l40)2h3+f$sx)qikpxn*f)dq%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestorlabapp'
+    'gestorlabapp',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- gestorlabproject/urls.py
+++ gestorlabproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestorlabapp.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('form/', form),
 ]
