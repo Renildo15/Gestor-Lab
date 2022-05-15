2bfab9b58664247d1161239e4650b434a38588f0
rickEDU <77811164+rickEDU@users.noreply.github.com>
2022-05-15 15:03:09 -0300

    correção do CRUD #18

--- laboratorio_app/urls.py
+++ laboratorio_app/urls.py
@@ -2,6 +2,8 @@ from os import name
 from django.urls import path
 from . import views
 
+app_name ='laboratorio'
+
 urlpatterns = [
     path('',views.home, name='home'),
     path('form/', views.form, name='form'),
