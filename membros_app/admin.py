a0ffa9a2bf757661b4a6843420ddb45979171c9d
brenolves <brenoalves43@hotmail.com>
2022-05-16 22:20:35 -0300

    Models adicionadas ao admin para testes #20

--- membros_app/admin.py
+++ membros_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Membro, Perfil
+from laboratorio_app.models import Lab
 
 # Register your models here.
 
 admin.site.register(Membro)
 admin.site.register(Perfil)
+admin.site.register(Lab)
