d18a79962178515f4222156d482ea273d47c2bef
leonardodantas4 <leonardodds@hotmail.com>
2022-05-20 10:00:10 -0300

    Models #21

--- projetos_app/models.py
+++ projetos_app/models.py
@@ -1,3 +1,13 @@
+from datetime import datetime
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+
+class Projeto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=150)
+    coordenador = models.CharField(max_length=50)
+    participantes = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.titulo
