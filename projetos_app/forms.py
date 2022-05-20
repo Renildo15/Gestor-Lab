ac98b657473369e9b78f87ecd3f185e7a56bb8de
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-05-20 15:52:21 -0300

    Adaptando o modulo #21

--- projetos_app/forms.py
+++ projetos_app/forms.py
@@ -6,4 +6,17 @@ from projetos_app.models import Projeto
 class ProjectForm(ModelForm):
     class Meta:
         model=Projeto
-        fields = ['nome','descricao','coordenador','participantes']
+        fields = '__all__'
+        labels = {
+            'nome': 'Nome',
+            'descricao': 'Descrição',
+            'coordenador': 'Coordenador',
+            'participantes': 'Participantes',
+            'laboratorio': 'Laboratório'
+        }
+
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm, self).__init__(*args, **kwargs)
+            self.fields['coordenador'].empty_label = "Selecione"
+            self.fields['participantes'].empty_label = "Selecione"
+            self.fields['laboratorio'].empty_label = "Selecione"
--- projetos_app/models.py
+++ projetos_app/models.py
@@ -1,12 +1,16 @@
+from mailbox import FormatError
+from tkinter import CASCADE
 from django.db import models
-from django.forms import CharField
+from membros_app.models import Membro
+from laboratorio_app.models import Lab
 
 
 class Projeto(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.CharField(max_length=150)
-    coordenador = models.CharField(max_length=50)
-    participantes = models.CharField(max_length=150)
+    coordenador_projeto = models.ForeignKey(Membro, on_delete=models.SET_NULL, related_name='coordenador_projeto', null=True, blank=True)
+    participantes = models.ForeignKey(Membro, on_delete=models.SET_NULL, related_name='participantes', null=True, blank=True)
+    laboratorio_projeto = models.ForeignKey(Lab, on_delete=models.CASCADE, related_name='laboratorio_projeto', null=True, blank=True)
 
     def __str__(self):
         return self.titulo
