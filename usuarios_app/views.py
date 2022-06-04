72e1a4dbddee78fadadeead4e54afe4a9a83101f
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-06-04 08:39:28 -0300

    Modificando o login #59

--- usuarios_app/views.py
+++ usuarios_app/views.py
@@ -13,4 +13,21 @@ def cadastro(request):
        form_usuario = UserCreationForm()
     return render(request,'usuarios/cadastro_user.html', {'form_usuario': form_usuario})
 
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        usuario =authenticate(request, username=username, password=password)
+
+        if usuario is not None:
+            login_process(request, usuario)
+            return redirect('PaginaInicial')
+        else:
+            form_login = AuthenticationForm()
+    else:
+        form_login = AuthenticationForm()
+    return render(request, 'usuarios/login_user.html', {'form_login': form_login})
+    
+
            
