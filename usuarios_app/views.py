308caa28afd971e112c7da6efda1da2a58b91e91
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-06-04 08:29:50 -0300

    Modificando cadastro #59

--- usuarios_app/views.py
+++ usuarios_app/views.py
@@ -1,35 +1,16 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login as login_process
 # Create your views here.
 def cadastro(request):
-    if request.method == "GET":
-        return render(request,'usuarios/cadastro_user.html')
+    if request.method == "POST":
+       form_usuario = UserCreationForm(request.POST)
+       if form_usuario.is_valid():
+           form_usuario.save()
+           return redirect('PaginaInicial')
     else:
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
+       form_usuario = UserCreationForm()
+    return render(request,'usuarios/cadastro_user.html', {'form_usuario': form_usuario})
 
-        user = User.objects.filter(username=username).first()
-
-        if user:
-            return HttpResponse('Ja exixte um usuario com esse username')
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        user.save()
-        return HttpResponse('Usuario cadastrado com sucesso')
-
-def login(request):
-
-    if request.method == "GET":
-        return render(request, 'usuarios/login_user.html')
-    else:
-        username = request.POST.get('username')
-        senha = request.POST.get('senha')
-        user = authenticate(username=username, password=senha)
-
-        if user is not None:
-            login_process(request, user)
-            return redirect('PaginaInicial')
-        else:
-            return HttpResponse('algo invalido')
+           
