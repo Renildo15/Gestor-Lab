14ac100b233bda0b8b20d4238977cbd418e64957
PedroAugustoMD <pedro.augustohp02@gmail.com>
2022-06-05 20:58:09 -0300

    Add login_required #61

--- horario_app/views.py
+++ horario_app/views.py
@@ -1,39 +1,44 @@
 from django.shortcuts import redirect, render
 from .forms import HorarioForm
 from .models import Horario
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='account_login')
 def home(request):
     data = {}
     data['db'] = Horario.objects.all()
     return render(request,'horario_index.html',data)
 
 
+@login_required(login_url='account_login')
 def form(request):
     data = {}
     data['form'] = HorarioForm()
     return render(request,'horario_form.html',data)
 
 
+@login_required(login_url='account_login')
 def create(request):
     form = HorarioForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('/horario/')
 
+@login_required(login_url='account_login')
 def view(request,pk):
     data = {}
     data['db'] = Horario.objects.get(pk=pk)
     return render(request, 'horario_view.html', data)
 
-
-
-
+@login_required(login_url='account_login')
 def edit(request, pk):
     data = {}
     data['db'] = Horario.objects.get(pk = pk)
     data['form'] = HorarioForm(instance=data['db'])
     return render(request,'horario_form.html', data)
 
+@login_required(login_url='account_login')
 def update(request, pk):
     data = {}
     data['db'] = Horario.objects.get(pk = pk)
@@ -42,6 +47,7 @@ def update(request, pk):
         form.save()
         return redirect('/horario/')
 
+@login_required(login_url='account_login')
 def delete(request, pk):
     db = Horario.objects.get(pk = pk)
     db.delete()
