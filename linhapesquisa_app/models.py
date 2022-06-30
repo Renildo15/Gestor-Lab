32c5455bfb9c4102327b5d98526b84c435c688a2
PedroAugustoMD <pedro.augustohp02@gmail.com>
2022-06-29 22:25:25 -0300

    Teste models linha de pesquisa #102

--- linhapesquisa_app/models.py
+++ linhapesquisa_app/models.py
@@ -4,7 +4,7 @@ from laboratorio_app.models import Lab
 # Create your models here.
 class LinhaPesquisa(models.Model):
     nome = models.CharField(max_length=60)
-    descricao = models.TextField()
+    descricao = models.TextField(max_length=200)
     areaCNPQ = models.CharField(max_length=150,null=True, blank=True)
     subAreaCNPQ = models.CharField(max_length=150,null=True, blank=True)
     laboratorio_pesq = models.ForeignKey(Lab,on_delete=models.CASCADE, related_name='laboratorio_pesq',null=True, blank=True)
