15d20c4963105592aa002de3012300b3246fd4f2
brenolves <brenoalves43@hotmail.com>
2022-07-02 11:24:44 -0300

    Corrigidos Security Hotspots, Code Smells e/ou bugs em gestorlabapp #112

--- gestorlabapp/urls.py
+++ gestorlabapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.PaginaInicial, name='PaginaInicial'),
+    path('',views.pagina_inicial, name='PaginaInicial'),
     path('sobre/', views.sobre, name='sobre'),
 ]
