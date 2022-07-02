d30ac3d08e7c94181656c187207c2013e8b575d8
brenolves <brenoalves43@hotmail.com>
2022-07-02 11:24:24 -0300

    Corrigidos Security Hotspots, Code Smells e/ou bugs em evento_app #112

--- evento_app/views.py
+++ evento_app/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import redirect, render
 from evento_app.forms import EventForm
 from evento_app.models import Evento
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_safe, require_http_methods
 
 redirLink = '/evento/'
 
-
+@require_safe
 def home(request):
     data = {}
     data['evt'] = Evento.objects.all()
     return render(request,'event_index.html',data)
 
+@require_safe
 @login_required(login_url='account_login')
 def form(request):
     data = {}
     data['form'] = EventForm()
     return render(request,'event_form.html',data)
 
+@require_http_methods(["POST"])
 @login_required(login_url='account_login')
 def create(request):
     form = EventForm(request.POST or None)
@@ -24,14 +27,14 @@ def create(request):
         form.save()
         return redirect(redirLink)
 
+@require_safe
 @login_required(login_url='account_login')
 def view(request,pk):
     data = {}
     data['evt'] = Evento.objects.get(pk=pk)
     return render(request, 'event_view.html', data)
 
-
-
+@require_safe
 @login_required(login_url='account_login')
 def edit(request, pk):
     data = {}
@@ -39,6 +42,7 @@ def edit(request, pk):
     data['form'] = EventForm(instance=data['evt'])
     return render(request,'event_form.html', data)
 
+@require_http_methods(["POST"])
 @login_required(login_url='account_login')
 def update(request, pk):
     data = {}
@@ -48,6 +52,7 @@ def update(request, pk):
         form.save()
         return redirect(redirLink)
 
+@require_http_methods(["POST"])
 @login_required(login_url='account_login')
 def delete(request, pk):
     db = Evento.objects.get(pk = pk)
