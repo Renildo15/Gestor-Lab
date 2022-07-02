1fe2145dff6f4edd7f1f9e14de0ded6e40c8da17
brenolves <brenoalves43@hotmail.com>
2022-07-02 11:25:44 -0300

    Corrigidos Security Hotspots, Code Smells e/ou bugs em linhapesquisa_app #112

--- linhapesquisa_app/views.py
+++ linhapesquisa_app/views.py
@@ -3,21 +3,24 @@ from django.shortcuts import redirect, render
 from linhapesquisa_app.forms import LinhaForm
 from linhapesquisa_app.models import LinhaPesquisa
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_safe, require_http_methods
 
 redirLink = '/linhapesquisa/'
 
-
+@require_safe
 def home(request):
     data = {}
     data['db'] = LinhaPesquisa.objects.all()
     return render(request,'linha_index.html',data)
 
+@require_safe
 @login_required(login_url='account_login')
 def form(request):
     data = {}
     data['form'] = LinhaForm()
     return render(request,'linha_form.html',data)
 
+@require_http_methods(["POST"])
 @login_required(login_url='account_login')
 def create(request):
     form = LinhaForm(request.POST or None)
@@ -25,13 +28,14 @@ def create(request):
         form.save()
         return redirect(redirLink)
 
+@require_safe
 @login_required(login_url='account_login')
 def view(request,pk):
     data = {}
     data['db'] = LinhaPesquisa.objects.get(pk=pk)
     return render(request, 'linha_view.html', data)
 
-
+@require_safe
 @login_required(login_url='account_login')
 def edit(request, pk):
     data = {}
@@ -39,6 +43,7 @@ def edit(request, pk):
     data['form'] = LinhaForm(instance=data['db'])
     return render(request,'linha_form.html', data)
 
+@require_http_methods(["POST"])
 @login_required(login_url='account_login')
 def update(request, pk):
     data = {}
@@ -47,7 +52,8 @@ def update(request, pk):
     if form.is_valid():
         form.save()
         return redirect(redirLink)
-
+    
+@require_safe
 @login_required(login_url='account_login')
 def delete(request, pk):
     db = LinhaPesquisa.objects.get(pk = pk)
