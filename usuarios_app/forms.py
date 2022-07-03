73733613630d1d6cb46867ecf4476469d6404436
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-07-02 23:42:11 -0300

    correções #115

--- usuarios_app/forms.py
+++ usuarios_app/forms.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label='Primeiro Nome')
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome')
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
