896c250608957a5d6feb4bec9863792f92888e4b
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-07-04 11:32:36 -0300

    Corrigindo STATIC_ROOT #116

--- gestorlabproject/settings.py
+++ gestorlabproject/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
         "USER": "postgres_user",
         "PASSWORD": "password",
         "HOST": "postgres",
+        "PORT": 5432,
     }
 }
 
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
