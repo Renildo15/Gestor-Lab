17c23a2889293ef44f197114b1976321c3b62af6
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-07-04 09:38:00 -0300

    Correção #96

--- evento_app/models.py
+++ evento_app/models.py
@@ -6,7 +6,7 @@ from laboratorio_app.models import Lab
 class Evento(models.Model):
     titulo = models.CharField(max_length=50)
     area = models.CharField(max_length=50)
-    laboratorio = models.ForeignKey(Lab, on_delete=models.CASCADE,null=True, blank=True )
+    laboratorio = models.ForeignKey(Lab, on_delete=models.CASCADE, null=True, blank=True)
     site = models.URLField(max_length=200)
     data_sbm = models.DateField(auto_now_add=True)
     descricao = models.TextField()
