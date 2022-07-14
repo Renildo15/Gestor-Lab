49e46d8d93241daff15871c68b7d420919874b3b
Renildo Rabi <renildorabi@ufrn.edu.br>
2022-07-14 13:15:17 -0300

    css login/register/password #130

--- usuarios_app/forms.py
+++ usuarios_app/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control input'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label='Primeiro Nome')
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label='Sobrenome')
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control input'}), label='Sobrenome')
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
@@ -13,11 +13,11 @@ class RegisterUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['first_name'].widget.attrs['class'] = 'form-control'
-        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['class'] = 'form-control input'
+        self.fields['password1'].widget.attrs['class'] = 'form-control input'
+        self.fields['password2'].widget.attrs['class'] = 'form-control input'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control input'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control input'
 
 class PasswordChangingForm(PasswordChangeForm):
     class Meta:
@@ -27,8 +27,8 @@ class PasswordChangingForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super(PasswordChangingForm, self).__init__(*args, **kwargs)
 
-        self.fields['old_password'].widget.attrs['class'] = 'form-control'
-        self.fields['new_password1'].widget.attrs['class'] = 'form-control'
-        self.fields['new_password2'].widget.attrs['class'] = 'form-control'
+        self.fields['old_password'].widget.attrs['class'] = 'form-control input'
+        self.fields['new_password1'].widget.attrs['class'] = 'form-control input'
+        self.fields['new_password2'].widget.attrs['class'] = 'form-control input'
 
 
